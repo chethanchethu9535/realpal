1268e69ab7065b164b5c6c57933858c584dc3763
chethan <chethanmcssvn@gmail.com>
2024-09-08 12:01:31 +0530

    create user update api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,11 @@
-import express from 'express';
-import { test } from '../controller/user.controller.js';
 
+import express from 'express';
+import { test, updateUser } from '../controller/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
-router.get('/test',test);
+router.get('/test', test);
+router.post('/update/:id', verifyToken, updateUser); // Correct 'routet' to 'router'
 
 export default router;
