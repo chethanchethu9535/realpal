47979a636d7af6b27130982d52fed81a59450975
chethan <chethanmcssvn@gmail.com>
2024-09-19 19:18:52 +0530

    Create the listing item component and show listings

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -103,7 +103,7 @@ export default function Listing() {
               </p>
               {listing.offer && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                  ${+listing.regularPrice - +listing.discountPrice}
+                  ${+listing.regularPrice - +listing.discountPrice}OFF
                 </p>
               )}
             </div>
@@ -135,7 +135,7 @@ export default function Listing() {
             </ul>
             {currentUser && listing.userRef !== currentUser._id && !contact && (
             <button  onClick={()=>setContact(true)} className='bg-slate-950 text-white rounded-lg uppercase hover:opacity-75 p-3' >contact landlord</button>)}
-            {contact && <Contact listing={listing}/>}
+            {contact && <Contact listing={listing} />}
           </div>
       
       </div>
